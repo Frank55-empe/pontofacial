b64dbb8672b17759b3bc00c4416e96b6dadb118d
Frank55-empe <159260196+Frank55-empe@users.noreply.github.com>
2026-08-06 15:24:40 -0300

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/ponto-facial/', // ajuste para o nome do seu repositório no GitHub Pages
+  base: '/pontofacial/', // ajuste para o nome do seu repositório no GitHub Pages
 })

184dbc160e2e2989053881c4a798e902795cee09
Frank55-empe <159260196+Frank55-empe@users.noreply.github.com>
2026-08-20 10:37:08 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 export const APPS_SCRIPT_URL =
       'https://script.google.com/macros/s/AKfycbzadsGtGm6bw3nU-x0s38IP_xO-3YaeWAsv4_uCnE833utsPSgGcR907wnW_Nx2J64P/exec';
-
     export const APP_CONFIG = {
       nomeEmpresa: 'Minha Empresa',
       limiarReconhecimentoFacial: 0.55,

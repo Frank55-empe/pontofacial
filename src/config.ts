38bc815df20f3d29ff6c45c011469639640a38ff
Frank55-empe <159260196+Frank55-empe@users.noreply.github.com>
2026-08-25 14:19:58 -0300

    Update APPS_SCRIPT_URL and add DISTANCIA_FACIAL_MAX
    Updated APPS_SCRIPT_URL to a new endpoint and added DISTANCIA_FACIAL_MAX constant.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const APPS_SCRIPT_URL =
   'https://script.google.com/macros/s/AKfycbzadsGtGm6bw3nU-x0s38IP_xO-3YaeWAsv4_uCnE833utsPSgGcR907wnW_Nx2J64P/exec';
-
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/SUA_URL_AQUI/exec';
+export const DISTANCIA_FACIAL_MAX = 0.5;
 export const APP_CONFIG = {
   nomeEmpresa: 'Minha Empresa',
   limiarReconhecimentoFacial: 0.55,
